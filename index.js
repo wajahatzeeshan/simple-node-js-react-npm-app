bffa222b9ec19c430a7a422adfa5fe4e3c6b3a2a
Tristan Hall <38442214+TrimHall@users.noreply.github.com>
2023-03-23 18:48:03 +0000

    updated function to show app

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const PORT = 3000;
 const htmlResponse = `<h1>Hello Lloyds Modern Engineering Participants!</h1>
 <p>This app was built by jenkins</p>`;
 
-app.get('/', (_req, res) => res.sendStatus(200).send(htmlResponse));
+app.get('/', (_req, res) => res.status(200).send(htmlResponse));
 
 app.listen(PORT, () => `App is listening on ${PORT}`);
